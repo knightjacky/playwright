ea6df173422e22b7b1d685f9e4d22c06800f551d
jacky yu <knight.jacky@gmail.com>
2025-02-09 14:09:49 -0500

    test

---
+++ pageObject/financialDataVisualization.js
@@ -0,0 +1,18 @@
+import HelperBase from "./helperBase";
+
+export default class FinancialDataVisualization extends HelperBase {
+  constructor(page) {
+    super(page);
+    // Define locators for login elements
+    this.usernameInput = page.locator('#username');
+    this.passwordInput = page.locator("#password");
+    this.loginButton = page.locator("#login-button");
+  }
+
+  async login(username, password) {
+    await this.usernameInput.fill(username); // Fill in the username
+    await this.passwordInput.fill(password); // Fill in the password
+    await this.loginButton.click(); // Click the login button
+    await this.waitForNumberOfSeconds(2);
+  }
+}
