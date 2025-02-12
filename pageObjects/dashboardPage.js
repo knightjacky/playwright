7c6e7db771e5cd3e2d0eeb1d0e5dd11d13053895
jacky yu <knight.jacky@gmail.com>
2025-02-12 17:07:44 -0500

    test

--- pageObjects/dashboardPage.js
+++ pageObjects/dashboardPage.js
@@ -1,16 +1,42 @@
-import HelperBase from "./helperBase.js";
+class DashboardPage
+{
+constructor(page)
+{
+    this.page = page;
+    this.products = page.locator(".card-body");
+    this.productsText = page.locator(".card-body b");
+    this.cart =  page.locator("[routerlink*='cart']");
+    this.orders = page.locator("button[routerlink*='myorders']");
 
-export default class DashboardPage extends HelperBase {
-  constructor(page) {
-    super(page);
-
-    // Define locators for login elements
-    this.dashboardTitle = page.locator('[data-cy="dashboard-title"]');
-  }
-
-  async login(username, password) {
-    await this.usernameInput.fill(username); // Fill in the username
-    await this.passwordInput.fill(password); // Fill in the password
-    await this.loginButton.click(); // Click the login button
-  }
 }
+
+async searchProductAddCart(productName)
+{
+   
+    const titles= await this.productsText.allTextContents();
+    console.log(titles);
+    const count = await this.products.count();
+    for(let i =0; i < count; ++i)
+    {
+    if(await this.products.nth(i).locator("b").textContent() === productName)
+    {
+        //add to cart
+        await this.products.nth(i).locator("text= Add To Cart").click();
+        break;
+     }
+    }
+}
+
+async navigateToOrders()
+{
+    await this.orders.click();
+}
+
+
+async navigateToCart()
+{
+    await this.cart.click();
+}
+
+}
+module.exports = {DashboardPage};
--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,33 +1,21 @@
-import { test, expect } from "@playwright/test";
-import PageObjectManager from "../pageObjects/pageOjectManager";
-// import faker from '@faker-js/faker';
+let message = "Hello";
+message = 5;
 
-test.describe("Login Tests", () => {
-  test.describe.configure({retries: 2});
-  test.beforeEach(async ({ page }) => {
-    const baseUrl = process.env.BASE_URL;
-    await page.goto("https://qa.brokerbay.com/");
-  });
+function add(a, b) {
+    return a + b;
+  }
+  add(3, "4"); 
 
-  test.only("Should log in with valid credentials @regression", async ({ page }, testInfo) => {
-    if(testInfo.retry){
-        // do something like clear the test data or delete some info reset the api if retry happening 
+
+  const person = { name: "Alice", age: 25 };
+  person.favoriteColor = "blue";
+
+  const { expect } = require("@playwright/test");
+
+
+  class Person {
+    constructor(name) {
+      this.name = name;
     }
-    const validUserName = "jacky+agent@brokerbay.com";
-    const validPassword = "Test123!";
-    const poManager = new PageObjectManager(page);
-    // const randomFullAddress = faker.randomFullAddress()
-    const apiRandomID = "#username";
-
-    // console.log("$$$$$$$$$$$"+randomFullAddress);
-    const loginPage =poManager.getLoginPage(apiRandomID)
-
-    loginPage.login(validUserName, validPassword);
-
-    // Add assertions to verify successful login (e.g., check for a dashboard element)
-    await poManager.getDatePickerPage().waitForNumberOfSeconds(5);
-    // await expect(usingTheGridForm).toHaveScreenshot({maxDiffPixels: 300});
-    // await expect(page.locator("#dashboard")).toBeVisible();
-    await page.waitForTimeout(5000);
-  });
-});
+  }
+  
